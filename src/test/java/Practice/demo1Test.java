9841c680972135486df0ee310f47ecbf22a3ebf1
asus <asus@DESKTOP-IFS27JH>
2026-01-09 15:04:13 +0530

    Committed

---
+++ src/test/java/Practice/demo1Test.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class demo1Test {
+
+	@Test(groups = "smoke")
+	public void HPTest()
+	{
+		System.out.println("Harry");
+	}
+	
+	
+}
---
+++ src/test/java/Practice/demoTest.java
@@ -0,0 +1,14 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class demoTest {
+
+	@Test(groups = "regression")
+	public void HP1Test()
+	{
+		System.out.println("Ron");
+	}
+	
+	
+}
